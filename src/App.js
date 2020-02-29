d9a08dc6d9770e3c5ad8e3d902f0b7a97a7a29e1
mkloar <miha.kloar@gmail.com>
2020-02-29 12:46:36 +0100

    Update header and rendering

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SearchCategory from './SearchCategory';
 class App extends Component {
   constructor() {
     super();
-    this.state={
+    this.state = {
       players: [],
       searchfield: '',
       category: 'name'
@@ -14,67 +14,61 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-    const prospectsUrl="https://statsapi.web.nhl.com/api/v1/draft/prospects";
-    const players1 = [];
+    const prospectsUrl = "https://statsapi.web.nhl.com/api/v1/draft/prospects";
+    const playersList = [];
 
     fetch(prospectsUrl)
-    .then((resp) => resp.json())
-    .then((data) => {
-    
-    for(let i=0; i < data.prospects.length/10; i++) {
-        let fullName1 = data.prospects[i].fullName;
-        let height1 = data.prospects[i].height;
-        let country1 = data.prospects[i].birthCountry;
-        let weight1 = data.prospects[i].weight;
-        let position1 = data.prospects[i].primaryPosition.abbreviation;
-        let birthDay1= data.prospects[i].birthDate;
+      .then((resp) => resp.json())
+      .then((data) => {
 
-        birthDay1 = new Date().getFullYear() - birthDay1.substring(0,4);
+        for (let i = 0; i < data.prospects.length / 10; i++) {
+          let fullName = data.prospects[i].fullName;
+          let height = data.prospects[i].height;
+          let country = data.prospects[i].birthCountry;
+          let weight = data.prospects[i].weight;
+          let position = data.prospects[i].primaryPosition.abbreviation;
+          let birthDay = data.prospects[i].birthDate;
 
-        if(country1 === undefined ) {
-          country1 = "no data";
-        }else if(country1 === "SVN") {
-          country1 = "SLO";
+          birthDay = new Date().getFullYear() - birthDay.substring(0, 4);
+
+          if (country === undefined) {
+            country = "no data";
+          } else if (country === "SVN") {
+            country = "SLO";
+          }
+
+          var player = {
+            fullName,
+            height,
+            country,
+            weight,
+            position,
+            birthDay
+          };
+          playersList.push(player);
         }
 
-        var obj = {
-          fullName: fullName1,
-          height: height1,
-          country: country1,
-          weight: weight1,
-          position: position1,
-          birthDay: birthDay1,
-        };
-        //console.log(obj);
-        players1.push(obj);
-        //ul.appendChild(li);
-    } 
-
-    console.log(players1);
-    this.setState({players: players1});
-    });
-    //this.setState({players: players1});
-    
-    //console.log(players1);
-
-
+        this.setState({ players: playersList });
+      });
 
   }
+
   onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value });
+    this.setState({ searchfield: event.target.value });
   }
+  
   onCategoryChange = (event) => {
-    this.setState({category: event.target.value});
+    this.setState({ category: event.target.value });
   }
 
   whichCategory = (player) => {
     const category = this.state.category;
     if (category === 'country') {
       const location = `${player.country}`;
-      return location.slice(0,4).toLowerCase();
+      return location.slice(0, 4).toLowerCase();
     }
     else if (category === 'name') {
-      return  player.fullName.toLowerCase();
+      return player.fullName.toLowerCase();
     }
     else if (category === 'position') {
       return player.position.toLowerCase();
@@ -85,10 +79,6 @@ class App extends Component {
   }
 
   render() {
-    const filteredPlayers1 = this.state.players.filter(player => {
-      return player.fullName.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    });
-
     const filteredPlayers = this.state.players.filter(player => {
       const category = this.whichCategory(player);
       if (category !== undefined) {
@@ -98,21 +88,17 @@ class App extends Component {
         return null;
       }
     });
+
     return (
       <div className="tc">
-        
         <header className="custom--unselectable fixed top-0 w-100 h3 white custom--bg-near-black custom--shadow-4 z-2">
-          
-          <h1 id="title" className="relative ma0 pa0 fl-l">NHL 2018 draft (only 9000/10 players)</h1>
-          <SearchCategory categoryChange={this.onCategoryChange}/>
-          <SearchBox searchChange={this.onSearchChange} searchCategory={this.state.category}/>
+          <h1 id="title" className="relative ma0 pa0 fl-l">NHL draft players</h1>
+          <SearchCategory categoryChange={this.onCategoryChange} />
+          <SearchBox searchChange={this.onSearchChange} searchCategory={this.state.category} />
         </header>
-
         <main className="flex-auto">
-          <PlayerList players={filteredPlayers}/>
+          <PlayerList players={filteredPlayers} />
         </main>
-      
-        
       </div>
     );
   }
--- src/Player.js
+++ src/Player.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Player = ({fullName, country, height, weight, position, birthDay, id}) => {
+const Player = ({fullName, country, height, weight, position, birthDay, id, style}) => {
 	return (
-		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robots' src={`https://robohash.org/${id}?200x200`} />
+		<div className='tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5' style={style}>
+			<img alt='robots' src={`https://robohash.org/${id}?200x200`} width={100} height={100} />
 			<div>
 				<h2>{fullName}</h2>
-				<p> {height} - {weight} ; {position} ; {birthDay} y/o</p>
+				<p> {height} - {weight}lbs; {position} ; {birthDay} y/o</p>
 				<p>{country}</p>
 			</div>
 		</div>
